a90d54419a53292a65963467e23e0b0b4fa3c488
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-03-02 17:44:00 +0200

    Added postgres env variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import  os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret_key'
 api = Api(app)
@@ -29,5 +31,5 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
-    db.init_app(app)
-    app.run(debug=False)
+    # db.init_app(app)
+    app.run(port= 5000)
